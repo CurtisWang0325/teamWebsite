20ac932bf44af5790f31b56a378c123ce2410b13
Chien Wang <wangc6@rose-hulman.edu>
2018-07-12 04:49:02 -0400

    brand new page

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <h1>Message</h1>
       </div>
     );
   }
