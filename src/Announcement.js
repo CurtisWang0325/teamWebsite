94f50d04503c4334620fe7f88a81ec74260251e4
Chien Wang <wangc6@rose-hulman.edu>
2018-07-16 04:16:40 -0400

    firebase config

--- src/Announcement.js
+++ src/Announcement.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
+import AnnouncementLists from './AnnouncementLists'
 
 class Announcement extends Component {
     render() {
         return (
+            <div>                
             <h1>Announcement!</h1>
+            <AnnouncementLists/>
+            </div>
         )
     }
 }
---
+++ src/AnnouncementLists.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class AnnouncementLists extends Component{
+    render(){
+        return (
+            <p>history</p>
+        )
+    }
+
+}
+
+export default AnnouncementLists
