166d25bd7be50d6020cabecf5b6a93f955627e5a
Chien Wang <wangc6@rose-hulman.edu>
2018-07-29 10:32:47 -0400

    add navlink to my account

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,22 +1,22 @@
 import React, {Component} from 'react'
 import SignOut from './SignOut'
+import { NavLink} from  'react-router-dom'
 
 class UserInfo extends Component{
   render(){
     return (
-      <div
-      className="UserInfo"
-      >
-      <div className="user">
-        <span 
-          //contentEditable='false'
-        >
-          {this.props.user.displayName}
-
-        </span>
+      <div className="UserInfo">
+        <div className="user">
+          <span 
+            //contentEditable='false'
+          >
+            {this.props.user.displayName}
+          </span>
+        </div>
+            <NavLink to="/myAccount"><button>My Account</button></NavLink>
+          
+        <SignOut signOut={this.props.signOut} />
       </div>
-       <SignOut signOut={this.props.signOut} />
-    </div>
     )
   }
 }
