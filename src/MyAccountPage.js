2f4a9a83d92853fb8a2e41a62daa517c711bce11
Chien Wang <wangc6@rose-hulman.edu>
2018-07-30 11:22:37 -0400

    change name after form submitted

--- src/MyAccountPage.js
+++ src/MyAccountPage.js
@@ -6,6 +6,7 @@ class MyAccountPage extends Component {
     
         this.state = {
             editMode:"false",
+            tempName:"",
             name:"",
         }
       }
@@ -15,7 +16,13 @@ class MyAccountPage extends Component {
     }
 
     handleChange = (ev) => {
-        this.setState({name:ev.target.value})
+        this.setState({tempName:ev.target.value})
+    }
+
+    handleSubmit = (ev) =>{
+        ev.preventDefault()
+        this.setState({name:this.state.tempName})
+        console.log(ev.target)
     }
 
     render() {
@@ -25,12 +32,12 @@ class MyAccountPage extends Component {
                 
                 <h1>{`Name: ${this.state.name?this.state.name:this.props.user.googleName}`}</h1>
 
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <input 
                             type="text"
                             placeholder="Enter new name"
-                            value={this.state.userName}
+                            value={this.state.tempName}
                             onChange={this.handleChange}
                             />
                     </div>
