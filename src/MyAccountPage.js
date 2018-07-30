7bb4f9f2a5041da6acff31fd5fc42ffb9d66b46b
Chien Wang <wangc6@rose-hulman.edu>
2018-07-30 11:11:37 -0400

    add control form in my account page

--- src/MyAccountPage.js
+++ src/MyAccountPage.js
@@ -1,9 +1,41 @@
 import React, {Component} from 'react'
 
 class MyAccountPage extends Component {
+    constructor() {
+        super()
+    
+        this.state = {
+            editMode:"false",
+            name:"",
+        }
+      }
+
+    editModeOn = ()=>{
+        this.setState({editMode:"true"})
+    }
+
+    handleChange = (ev) => {
+        this.setState({name:ev.target.value})
+    }
+
     render() {
         return (
-            <h1>{`Name: ${this.props.user.googleName}`}</h1>
+            <span>
+                <button onClick={this.editModeOn}>Edit</button>
+                
+                <h1>{`Name: ${this.state.name?this.state.name:this.props.user.googleName}`}</h1>
+
+                <form>
+                    <div>
+                        <input 
+                            type="text"
+                            placeholder="Enter new name"
+                            value={this.state.userName}
+                            onChange={this.handleChange}
+                            />
+                    </div>
+                </form>
+            </span>
         )
     }
 }
