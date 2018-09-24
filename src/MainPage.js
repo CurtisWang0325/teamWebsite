68876c62050a95f5112e2077bd4085943676b09e
Chien Wang <wangc6@rose-hulman.edu>
2018-09-24 17:37:59 -0400

    add instruction on main page

--- src/MainPage.js
+++ src/MainPage.js
@@ -4,9 +4,36 @@ import { NavLink } from 'react-router-dom'
 require('./css/MainPage.css')
 
 class MainPage extends Component {
+    state = {
+        showIns: false,
+    }
+
+    showOrHideIns = () => {
+        this.setState({ showIns: !this.state.showIns })
+    }
+
     render() {
         return (
             <div className="MainPage">
+                <button type='button' onClick={this.showOrHideIns} title='show/hide the instruction'>
+                    Show/Hide
+                </button>
+                {
+                    this.state.showIns ?
+                        <span>
+                            <h2>
+                                Sign in if you want to chat in forum or join events
+                        <br />
+                                Then click on "My Account" to fill the information
+                        <br />
+                                A complete profile helps you to interact with the website
+                        <br />
+                            </h2>
+                            <hr />
+                        </span>
+                        :
+                        null
+                }
 
                 <NavLink to="/Forum/general" className="link" id="to_forum">
                     <div className="unhover">
